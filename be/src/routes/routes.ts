68904c30bf5151cc3b748ad79bd16939f9a54323
lucanicoladebiasi <luca.nicola.debiasi@gmail.com>
2025-04-05 19:40:43 +0200

    be: /login end-point

--- be/src/routes/routes.ts
+++ be/src/routes/routes.ts
@@ -1,7 +1,6 @@
 import {Request, Response, Router} from 'express';
-import {Login, Phone} from "../dto";
+import {Login, Signin, Wallet} from "../dto";
 import mongoose from "mongoose";
-
 
 const mongo = async (): Promise<void> => {
     try {
@@ -16,25 +15,48 @@ mongo();
 
 const router: Router = Router();
 
-router.post('/login', async (req: Request, res: Response): Promise<void> => {
+router.post('/signin', async (req: Request, res: Response): Promise<void> => {
     try {
-        const login = req.body as Login;
-        console.log(req.body);
-        const isNew = !await Phone.exists({imsi: login.imsi});
+        const signin = req.body as Signin;
+        console.log(JSON.stringify(signin));
+        const isNew = !await Wallet.exists({phone: signin.phone});
         if (isNew) {
-            await Phone.create({imsi: login.imsi});
-            res.status(201).json(JSON.stringify(login));
+            await Wallet.create({phone: signin.phone, wallet: signin.wallet});
+            res.status(201).json({created: JSON.stringify(signin)});
         } else {
-            res.status(200).json(JSON.stringify(login));
+            res.status(200).json({ok: JSON.stringify(signin)});
         }
     } catch (error) {
         res.status(500).json({message: "Internal Server Error"});
     }
 })
 
-router.get('/reset', async (_req: Request, res: Response): Promise<void> => {
+router.post('/login', async (req: Request, res: Response): Promise<void> => {
     try {
-        await Phone.deleteMany();
+        const login = req.body as Login;
+        const wallet = await Wallet.findOne({wallet: login.wallet});
+        if (wallet) {
+            res.status(200).json({ok: JSON.stringify(wallet)});
+        } else {
+            res.status(404).json({not_found: JSON.stringify(login)});
+        }
+    } catch (error) {
+        res.status(500).json({message: "Internal Server Error"});
+    }
+})
+
+router.get('/login/verify', async (req: Request, res: Response): Promise<void> => {
+    try {
+        console.log("verify");
+        res.status(200).json({message: "verify"});
+    } catch (error) {
+        res.status(500).json({message: "Internal Server Error"});
+    }
+})
+
+router.delete('/db', async (_req: Request, res: Response): Promise<void> => {
+    try {
+        await Wallet.deleteMany();
         res.status(200).json({message: "Reset"});
     } catch (error) {
         res.status(500).json({message: "Reset Error"});
